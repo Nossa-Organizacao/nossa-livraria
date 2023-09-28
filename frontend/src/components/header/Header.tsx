816662c622c87bbe6b5e46074d0868ba51620439
rchsilver <rchsilver@gmail.com>
2023-09-28 10:05:48 -0300

    feat: new style header and main

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,42 +1,47 @@
-import { Button } from "../button/ButtonDefault";
+// import { Button } from "../button/ButtonDefault";
 import { HeaderStyle } from "./HeaderStyle";
 import IconSite from "../../assets/newChapter.png";
 import { GiArchiveResearch } from "react-icons/gi";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const path = window.location.pathname;
+  const isPageWithoutSearch =
+    path === "/login" || path === "/register" ? false : true;
+  const navigate = useNavigate();
+  const handleRegisterClick = () => {
+    navigate("/register");
+  };
+  const handleLoginClick = () => {
+    navigate("/login");
+  };
   return (
     <HeaderStyle>
       <div>
         <img src={IconSite} alt="Icone" />
-        {/* <span>Deixe suas histórias serem vista!</span> */}
-        <form action="submit">
-          <input type="text" placeholder="pequisar obra ou autor..." />
-          <button>
-            <GiArchiveResearch />
-          </button>
-        </form>
+        {isPageWithoutSearch && (
+          <section>
+            <form action="submit">
+              <input type="text" placeholder="pequisar obra ou autor..." />
+              <button>
+                <GiArchiveResearch />
+              </button>
+            </form>
+          </section>
+        )}
 
-        <section>
-          <Button
-            type="button"
-            $border={true}
-            $background="green-02"
-            $hoverBackground="green-01"
-            $width={3}
-          >
-            Criar Conta
-          </Button>
-
-          <Button
-            type="button"
-            $border={true}
-            $background="green-02"
-            $hoverBackground="green-01"
-            $width={3}
-          >
-            Logar
-          </Button>
-        </section>
+        <nav>
+          <ol>
+            <li>Lista de Obras</li>
+            <li>Crie sua Obra</li>
+            {path !== "/login" && (
+              <li onClick={() => handleLoginClick()}>Logar</li>
+            )}
+            {path !== "/register" && (
+              <li onClick={() => handleRegisterClick()}>Registrar</li>
+            )}
+          </ol>
+        </nav>
       </div>
     </HeaderStyle>
   );
--- frontend/src/components/header/HeaderStyle.ts
+++ frontend/src/components/header/HeaderStyle.ts
@@ -2,9 +2,12 @@ import { styled } from "styled-components";
 
 const HeaderStyle = styled.header`
   display: flex;
+  position: relative;
+  flex-direction: column;
   align-items: center;
   width: 100%;
   height: 90px;
+  padding: 5px;
   background-color: var(--color-brown-03);
 
   /* position: absolute;
@@ -31,31 +34,66 @@ const HeaderStyle = styled.header`
       width: 130px;
     }
 
-    form {
-      display: flex;
-      border-radius: 15px;
-      background-color: var(--color-white);
-      border: none;
-      padding: 5px;
+    nav {
+      ol {
+        display: flex;
+        gap: 0.7rem;
+        padding-top: 10px;
 
-      input {
-        font-size: var(--font-body-1);
-        margin-left: 15px;
-        border: none;
-        border-radius: 15px 0px 0px 15px;
+        li {
+          padding: 0.3rem;
+          border-radius: 15px;
+          background-color: var(--color-branch-00);
+          color: var(--color-brown-03);
+          font-size:var(--font-body-1);
+          font-weight: bold;
+          border: 3px solid var(--color-brown-03);
+        }
+
+        li:hover {
+          cursor: pointer;
+          transition: 0.4s;
+          border: 3px solid var(--color-branch-00);
+          color: var(--color-brown-02);
+        }
       }
+    }
+    section {
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 165px;
+      margin: auto;
+      width: 100px;
+      height: 50px;
 
-      button {
-        margin-inline: 10px;
-        border: none;
+      form {
+        display: flex;
+        border-radius: 15px;
         background-color: var(--color-white);
-        color: var(--color-brown-03);
-        font-size: 25px;
-        border-radius: 0px 15px 15px 0px;
-        cursor: pointer;
-      }
-      button:hover {
-        color: var(--color-green-01);
+        border: none;
+        padding: 5px;
+
+        input {
+          font-size: var(--font-body-1);
+          margin-left: 15px;
+          border: none;
+          border-radius: 15px 0px 0px 15px;
+        }
+
+        button {
+          margin-inline: 10px;
+          border: none;
+          background-color: var(--color-white);
+          color: var(--color-brown-03);
+          font-size: 25px;
+          border-radius: 0px 15px 15px 0px;
+          cursor: pointer;
+        }
+        button:hover {
+          color: var(--color-green-01);
+        }
       }
     }
   }
